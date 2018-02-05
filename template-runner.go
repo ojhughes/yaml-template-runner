5a74d7fa0f29288e71ffcafcd6af4ffffe48fa83
Ollie Hughes <ohughes@pivotal.io>
2018-02-05 17:02:51 +0000

    Add CLI flags for data and template files

--- template-runner.go
+++ template-runner.go
@@ -7,9 +7,9 @@ import (
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
 
-	"fmt"
-
 	"strings"
+	"path/filepath"
+	"flag"
 )
 
 type SubBlock struct {
@@ -20,6 +20,7 @@ type SubBlock struct {
 
 type Block struct {
 	Name string `yaml:"name"`
+	Template string `yaml:"template"`
 	Vars map[string]string `yaml:"vars,omitempty"`
 	SubBlocks []SubBlock `yaml:"subBlocks,omitempty"`
 
@@ -29,39 +30,44 @@ type TemplateDataSchema struct {
 	Blocks []Block
 }
 
-const pipelineTemplateName = "pipelineTemplateName"
+const (
+	pipelineTemplateName   = "pipelineTemplateName"
+)
 
 func main() {
-	dataFile, err := ioutil.ReadFile("resources/data-remove-tile.yml")
+	dataFilePath := flag.String("d", "", "Path to YAML templating schema definition")
+	templateFilePath := flag.String("t", "", "Path to layout template")
+	templatesDirectoryPath := flag.String("r", "resources", "Path to layout template")
+	flag.Parse()
+
+	dataFile, err := ioutil.ReadFile(filepath.Join(*templatesDirectoryPath, *dataFilePath))
 	check(err)
 
 	var dataSchema TemplateDataSchema
 	err = yaml.Unmarshal(dataFile, &dataSchema)
 	check(err)
 
-	blockTemplateData, err := ioutil.ReadFile("resources/block-remove-tile.yml")
 	check(err)
 
-	pipelineTemplateData, err := ioutil.ReadFile("resources/template-pipeline-laundromat.yml")
+	pipelineTemplateData, err := ioutil.ReadFile(filepath.Join(*templatesDirectoryPath, *templateFilePath))
 	check(err)
 
-	pipelineTemplate := template.New(pipelineTemplateName).Funcs(template.FuncMap{"indent": func(spaces int, v string) string {
-		pad := strings.Repeat(" ", spaces)
-		return pad + strings.Replace(v, "\n", "\n"+pad, -1)
-	}})
+	pipelineTemplate := template.New(pipelineTemplateName).Funcs(template.FuncMap{"indent": indent})
+
 
 	flattenedData := make(map[string]map[string]string)
 	for _, block := range dataSchema.Blocks  {
+		blockTemplateData, err := ioutil.ReadFile(filepath.Join(*templatesDirectoryPath, block.Template))
+		check(err)
 		blockVars := make(map[string]string)
-		template.Must(pipelineTemplate.New(block.Name).Parse( string(blockTemplateData)))
+		template.Must(pipelineTemplate.New(block.Name).Parse(string(blockTemplateData)))
 		for _, subBlock := range block.SubBlocks  {
-			template.Must(pipelineTemplate.New(subBlock.Name).ParseFiles("resources/" + subBlock.Template))
+			template.Must(pipelineTemplate.New(subBlock.Name).ParseFiles(filepath.Join(*templatesDirectoryPath, subBlock.Template)))
 			addAllEntries(subBlock.Vars, blockVars)
 		}
 		addAllEntries(block.Vars, blockVars)
 		flattenedData[block.Name] = blockVars
 	}
-	fmt.Print(pipelineTemplate.DefinedTemplates())
 	template.Must(pipelineTemplate.New(pipelineTemplateName).Parse(string(pipelineTemplateData)))
 	err = pipelineTemplate.ExecuteTemplate(os.Stdout, "pipelineTemplate", flattenedData)
 	check(err)
@@ -78,6 +84,11 @@ func check(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func indent(spaces int, v string) string {
+	pad := strings.Repeat(" ", spaces)
+	return pad + strings.Replace(v, "\n", "\n"+pad, -1)
 }
 
 
